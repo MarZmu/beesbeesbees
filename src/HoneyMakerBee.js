4ec787d5663b76119e7326ef483c35276db8a3ba
Marshall Zmuda <marshall.z@outlook.com>
2022-06-06 11:02:07 -0700

    Pass tests on Grub/Bee/HMBee/FBee/RetFBee.js

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
-var HoneyMakerBee = function() {
+var HoneyMakerBee = function(age) {
+  Grub.call(this, age || 10);
+  this.color = 'yellow';
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
